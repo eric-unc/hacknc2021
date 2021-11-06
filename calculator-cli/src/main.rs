1fc47c054a0d8ce7f524809be5c64851c8389a92
Eric Schneider <u1643364@live.unc.edu>
2021-11-06 12:52:46 -0400

    Semi-functional CLI

--- calculator-cli/src/main.rs
+++ calculator-cli/src/main.rs
@@ -1,5 +1,29 @@
+use std::io::{self, Write};
+use std::process::exit;
+
 use calculator::calculate;
 
 fn main() {
-    println!("Hello, {}!", calculate("5").unwrap());
+    println!("Welcome to our calculator! You can put \"quit\" to quit.");
+    loop {
+        print!(">>> ");
+        io::stdout().flush().expect("Failed to flush!");
+
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).expect("Failed to read line!");
+        line = line.trim().to_string();
+
+        match line.as_str() {
+            "" => continue,
+            "q" | "quit" | "exit" => exit(0),
+            expr => {
+                match calculate(expr) {
+                    Ok(n) => println!("{}", n),
+                    Err(e) => eprintln!("{}", e)
+                }
+            }
+        }
+
+        io::stdout().flush().expect("Failed to flush!");
+    }
 }
